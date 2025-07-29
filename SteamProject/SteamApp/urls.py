3ce050155b71c33265f67e21624de3adeb04afce
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-29 15:48:01 -0500

    Views Profile :D

--- SteamProject/SteamApp/urls.py
+++ SteamProject/SteamApp/urls.py
@@ -3,21 +3,21 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views.UserViews import RegisterUserViewSet, UsersViewSet
 from .views.GamesViews import JuegosViewSet
-from .views.ProfileViews import ProfileViewSet
+from .views.ProfileViews import EditProfileViewSet, PublicProfileViewSet
 
 
 router = DefaultRouter()
 router.register(r'register', RegisterUserViewSet, basename='register')
 router.register(r'users', UsersViewSet, basename='users')
 router.register(r'juegos', JuegosViewSet, basename='juegos')
-router.register(r'profiles', ProfileViewSet, basename='profiles')
+router.register(r'profiles', PublicProfileViewSet, basename='profiles')
 
 
 urlpatterns = [
     path('', include(router.urls)),
 
     path('juegos/<int:pk>/', JuegosViewSet.as_view({'put': 'put', 'delete': 'delete'}), name='juego-detail'),
-
+    path('editprofile/<int:pk>/', EditProfileViewSet.as_view({'put': 'put'}), name='profile-detail'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- SteamProject/SteamApp/views/ProfileViews.py
+++ SteamProject/SteamApp/views/ProfileViews.py
@@ -1,7 +1,47 @@
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from ..serializers import ProfileSerializer
 from ..models.ProfileModels import Profile
-from rest_framework import viewsets
 
-class ProfileViewSet(viewsets.ModelViewSet):
-    serializer_class = ProfileSerializer
+class PublicProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [AllowAny]
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            queryset = Profile.objects.all()
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'no estas autenticado'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class EditProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['put']
+
+    def put(self, request, *args, **kwargs):
+        user = request.user.id
+        if request.user.is_authenticated:
+            if request.user.is_superuser or self.request.user.rol == "ADMIN":
+                instance = self.get_object()
+                serializer = self.get_serializer(instance, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            elif user != self.get_object().user.id:
+                return Response({"error": "No tienes permiso para actualizar esta tarea."}, status=status.HTTP_403_FORBIDDEN)
+            elif user:
+                instance = self.get_object()
+                serializer = self.get_serializer(instance, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Usuario no autenticado"}, status=status.HTTP_401_UNAUTHORIZED)
