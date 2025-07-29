3718ed5066f83266d8f1bb27ca5d5aa45a2696e2
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-29 12:28:12 -0500

    Profile Api 100%

--- SteamProject/SteamApp/admin.py
+++ SteamProject/SteamApp/admin.py
@@ -3,3 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 from .models.UserModels import User
+from .models.ProfileModels import Profile
+from .models.GamesModels import Juego
+from .models.BibliotecaModels import Biblioteca, JuegoBiblioteca
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Juego)
+admin.site.register(Biblioteca)
+admin.site.register(JuegoBiblioteca)
--- SteamProject/SteamApp/models/GamesModels.py
+++ SteamProject/SteamApp/models/GamesModels.py
@@ -4,7 +4,7 @@ class Juego(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
-    imagen = models.URLField(default="https://placeholder.com/juego.jpg")
+    imagen = models.URLField(default="https://vanacco.com/wp-content/uploads/2022/01/Videogame-claves-1-1200x1200.jpg")
     fecha_lanzamiento = models.DateField(null=True, blank=True)
     desarrollador = models.CharField(max_length=100)
     
