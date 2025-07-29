2b7d53f83845fae6ea6973ddbfb4b11df19789bc
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-29 12:41:49 -0500

    Lectura del signals en el app.py

--- SteamProject/SteamApp/apps.py
+++ SteamProject/SteamApp/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class SteamappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SteamProject.SteamApp'
+
+    def ready(self):
+        import SteamProject.SteamApp.signals
